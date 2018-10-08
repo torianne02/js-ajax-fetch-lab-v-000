d61ee58e43fcfddfaebe9454cf0641f7800a6047
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-08 19:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const baseURL = 'https://api.github.com';
+const user = 'torianne02';
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
@@ -6,7 +9,15 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  const url = `${baseURL}/repos/${repo}/forks`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(res => res.json());
+  .then(json => showResults(json));
 }
 
 function showResults(json) {
@@ -20,3 +31,5 @@ function createIssue() {
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
 }
+
+// fbed3cac74745e55cdc9135c1a21da4b86e73996
