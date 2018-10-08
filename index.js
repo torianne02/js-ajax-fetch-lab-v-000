81c6e06dc4a1dd0b514a13414acc3cca2d54a7a9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-08 19:05:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ function createIssue() {
     title: document.getElementById('title').value,
     body: document.getElementById('body').value
   };
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
 }
 
 function getIssues() {
